2f0dd5a7c2be288e69830dc60a51a99153f05d1e
2Juicy <j0hnnywchiang@gmail.com>
2018-05-12 23:47:39 -0700

    started on JavaScript

---
+++ javascript/game.js
@@ -0,0 +1,11 @@
+//global variables list
+
+//Document check
+$(document).ready(function() {
+    $(".container").on("click", ".item", function() {
+   		userPick = $(this).val();
+
+
+
+    }); // End of .on(click) function
+}); // Document.ready ends here.
