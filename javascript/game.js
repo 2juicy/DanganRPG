4f77a3045adba78bf1680e6b4d60f4a6c1d0c0f9
2Juicy <j0hnnywchiang@gmail.com>
2018-05-13 13:54:36 -0700

    more jQuery and style done

--- javascript/game.js
+++ javascript/game.js
@@ -1,35 +1,34 @@
 //character stats and global variables
-const characters = {
-    Kaede: {
+const characters = ['Kaede', 'Kirumi', 'Miu', 'Monokuma'];
+const Kaede = {
     health: 100,
     attack: 10,
     counter: 15,
-},
-    Kirumi: {
+};
+const Kirumi = {
     health: 120,
     attack: 8,
     counter: 17
-},
-    Miu: {
+};
+const Miu = {
     health: 150,
     attack: 6,
     counter: 19,
-},
-    Monokuma: {
+};
+const Monokuma = {
     health: 180,
-    attack: 6,
+    attack: 5,
     counter: 22,
-    }
-}
-console.log(characters.Kaede.health);
-console.log(characters.Monokuma.attack);
+    };
+console.log(Kaede.health);
+console.log(Monokuma.attack);
 //Document check
 $(document).ready(function() {
+    $('.enemy').hide();
     //functions
-   
-
     
-    //song
+    
+    //song ignore this it was just for fun lol
     var audioElement = document.createElement("audio");
         audioElement.setAttribute("src", "images/soundtrack.mp3");    
     $(".column").on("click", "#character1", function() {
@@ -52,12 +51,26 @@ $(document).ready(function() {
     //on.click starts here for character selection, hides unselected characters.
     $(".container").on("click", ".select", function() {
     // $(".container").on("click", "#character1", function() {
+        const hideChosen = function(x){
+        if (x == 0){
+            $('#enemy1').hide();
+        } else if (x == 1){
+            $('#enemy2').hide();
+        } else if (x == 2){
+            $('#enemy3').hide();
+        } else {
+            $('#enemy4').hide();
+        }}
    		var userPick = $(this).val();
+        var characterSelected = characters[userPick];
         //hides unclicked buttons and keeps character selection.
         $('.character').toggle();
         $(this).show();
-        // $(this).hide();
+        //shows enemies
+        $('.enemy').show();
+        hideChosen(userPick);
         console.log(userPick);
+        console.log(characterSelected);
 
         var characterBtn = $("<button>");
 
