2e4d10499ee6e26479941daf71d7bb13d780e5e9
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-12 23:46:51 -0800

    revisiting old projects

--- javascript/game.js
+++ javascript/game.js
@@ -19,91 +19,109 @@ var clickOn = false;
 var enemySlain = 0;
 //Document check
 $(document).ready(function() {
-    $("#winOrLose").text('Select your character');
-    $('.enemy').hide();
-    $('.defender').hide();
-    $('#reset').hide();
-    //on.click starts here for character selection, hides unselected characters.
-    $(".container").on("click", ".select", function() {
-        if (countSelect === 1){
-            return;
-        }
-        const hideChosen = function(x){
-            if (x == 0){
-                $('#enemy1').hide();
-            } else if (x == 1){
-                $('#enemy2').hide();
-            } else if (x == 2){
-                $('#enemy3').hide();
-            } else {
-                $('#enemy4').hide();
-            }}
-   		let userPick = $(this).val();
-        characterSelected = characters[userPick];
-        //hides unclicked buttons and keeps character selection.
-        $('.character').hide();
-        $(this).slideDown();
-        //shows enemies
-        $('.enemy').show();
-        hideChosen(userPick);
-        countSelect++;
-        $("#winOrLose").text('Select your opponent! Defeat all enemies to win!');
-    });// End of .on(click) function, start enemy selection and hide.
-    $(".container").on("click", ".enemy", function() {
-        if (countEnemy === 1){
-            return;
-        }
-        $("#winOrLose").text('');
-        $(this).hide();
-        const showChosen = function(x){
-            if (x == 0){
-                $('#defender1').slideDown();
-            } else if (x == 1){
-                $('#defender2').slideDown();
-            } else if (x == 2){
-                $('#defender3').slideDown();
-            } else {
-                $('#defender4').slideDown();
-            }}
-        let userPick = $(this).val();
-        enemySelected = characters[userPick];
-        $(".enemyhp").text(enemySelected.health);
-        showChosen(userPick);
-        countEnemy++;
-        clickOn = true;
-    });// End of .on(click) function, begin on.click section to attack.
-    $('#attack').on('click', function(){
-        if (clickOn === false){
-            $("#winOrLose").text('No enemy here');
-            return;
-        }
-        powerUp = characterSelected.attack;
-        totalAttack += powerUp;
-        characterSelected.health -= enemySelected.counter;
-        $(".yourhp").text(characterSelected.health);
-        enemySelected.health -= totalAttack;
-        $(".enemyhp").text(enemySelected.health);
-        $("#winOrLose").text('You did ' + totalAttack + ' damage! Your enemy did ' + enemySelected.counter + ' damage to you!');
-        if (enemySelected.health <= 0){
-            $("#winOrLose").text('You did ' + totalAttack + ' damage! You defeated your enemy!');
-            characterSelected.health += enemySelected.counter;
-            $(".yourhp").text(characterSelected.health);
-            $('.defender').fadeOut(666);
-            countEnemy = 0;
-            enemySlain++;
-            clickOn = false;
-            if (enemySlain === 3){
-                $('#reset').slideDown(666);
-                $('#attack').slideUp(155);
-                $("#winOrLose").text('Victory! Game Over!');
-            }
-        }
-        if (characterSelected.health <= 0){
-            $("#winOrLose").text('Defeat! Game Over!');
-            $(".yourhp").text('0');
-            $('#reset').slideDown(666);
-            $('#attack').slideUp(155);
-            countEnemy = 1;
-        }
-    });//end of attack click
+  $("#winOrLose").text("Select your character");
+  $(".enemy").hide();
+  $("#enemies").hide();
+  $(".defender").hide();
+  $("#defenders").hide();
+  $("#reset").hide();
+  //on.click starts here for character selection, hides unselected characters.
+  $(".container").on("click", ".select", function() {
+    $("#enemies").show();
+    if (countSelect === 1) {
+      return;
+    }
+    const hideChosen = function(x) {
+      if (x == 0) {
+        $("#enemy1").hide();
+      } else if (x == 1) {
+        $("#enemy2").hide();
+      } else if (x == 2) {
+        $("#enemy3").hide();
+      } else {
+        $("#enemy4").hide();
+      }
+    };
+    let userPick = $(this).val();
+    characterSelected = characters[userPick];
+    //hides unclicked buttons and keeps character selection.
+    $(".character").hide();
+    $(this).slideDown();
+    //shows enemies
+    $(".enemy").show();
+    hideChosen(userPick);
+    countSelect++;
+    $("#winOrLose").text("Select your opponent! Defeat all enemies to win!");
+  }); // End of .on(click) function, start enemy selection and hide.
+  $(".container").on("click", ".enemy", function() {
+    $("#defenders").show();
+    if (countEnemy === 1) {
+      return;
+    }
+    $("#winOrLose").text("");
+    $(this).hide();
+    const showChosen = function(x) {
+      if (x == 0) {
+        $("#defender1").slideDown();
+      } else if (x == 1) {
+        $("#defender2").slideDown();
+      } else if (x == 2) {
+        $("#defender3").slideDown();
+      } else {
+        $("#defender4").slideDown();
+      }
+    };
+    let userPick = $(this).val();
+    enemySelected = characters[userPick];
+    $(".enemyhp").text(enemySelected.health);
+    showChosen(userPick);
+    countEnemy++;
+    clickOn = true;
+    if (enemySlain === 2) {
+      $("#enemies").hide();
+    }
+  }); // End of .on(click) function, begin on.click section to attack.
+  $("#attack").on("click", function() {
+    if (clickOn === false) {
+      $("#winOrLose").text("No enemy here");
+      return;
+    }
+    powerUp = characterSelected.attack;
+    totalAttack += powerUp;
+    characterSelected.health -= enemySelected.counter;
+    $(".yourhp").text(characterSelected.health);
+    enemySelected.health -= totalAttack;
+    $(".enemyhp").text(enemySelected.health);
+    $("#winOrLose").text(
+      "You did " +
+        totalAttack +
+        " damage! Your enemy did " +
+        enemySelected.counter +
+        " damage to you!"
+    );
+    if (enemySelected.health <= 0) {
+      $("#winOrLose").text(
+        "You did " + totalAttack + " damage! You defeated your enemy!"
+      );
+      characterSelected.health += enemySelected.counter;
+      $(".yourhp").text(characterSelected.health);
+      $(".defender").fadeOut(666);
+      countEnemy = 0;
+      enemySlain++;
+      clickOn = false;
+      $("#enemies").show();
+      if (enemySlain === 3) {
+        $("#reset").slideDown(666);
+        $("#attack").slideUp(155);
+        $("#winOrLose").text("Victory! Game Over!");
+      }
+    }
+    if (characterSelected.health <= 0) {
+      $("#winOrLose").text("Defeat! Game Over!");
+      $(".yourhp").text("0");
+      $("#reset").slideDown(666);
+      $("#attack").slideUp(155);
+      countEnemy = 1;
+    }
+  }); //end of attack click
 }); // Document.ready ends here.
