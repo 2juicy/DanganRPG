8a847c1e346dccb4987f1755b2d2ffc173f422f7
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-18 21:10:33 -0800

    add HP

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "Card.css";
 
-export default function Card({ style, id }) {
+export default function Card({ style, id, HP }) {
   return (
     <button className={`character ${style}`} id={`character${id}`} value={id}>
-      Kaede Akamatsu<span class="yourhp">100</span>
+      Kaede Akamatsu<span class="yourhp">{HP}</span>
     </button>
   );
 }
