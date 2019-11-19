5a36e21750951d3e45c30d0377b50a263972c24f
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-18 21:12:14 -0800

    all props needed

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "Card.css";
 
-export default function Card({ style, id, HP }) {
+export default function Card({ style, id, name, HP }) {
   return (
     <button className={`character ${style}`} id={`character${id}`} value={id}>
-      Kaede Akamatsu<span class="yourhp">{HP}</span>
+      {name}
+      <span class="yourhp">{HP}</span>
     </button>
   );
 }
