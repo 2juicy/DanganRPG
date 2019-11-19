45b362b298b20f8b304af27f4f9ec19e0c9049bc
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-18 21:19:07 -0800

    naming

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import characters from "./characters.json";
 import Board from "./components/Board/Board";
 
 function App() {
+  console.log(characters);
   return (
-    <div class="row">
+    <div className="row">
       <Board />
     </div>
   );
